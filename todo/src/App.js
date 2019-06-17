b9882fc2bb343642bd75ed18addb578455d63a72
Michael Harris <mhguitar1997@gmail.com>
2019-06-17 15:10:23 -0700

    Editied mapStateToProps function

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import './App.css';
-import {connect} from 'react-redux'
+import React from "react";
+import "./App.css";
+import { connect } from "react-redux";
 
 function App() {
   return (
     <div className="App">
-
+      {this.props.todos.map(todo => {
+        <h2>{todo.value}</h2>;
+      })}
     </div>
   );
 }
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+};
 
 export default App;
