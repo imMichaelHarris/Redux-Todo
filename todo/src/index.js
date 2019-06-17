5ba5769da07c3d932a8f76ce593e869580d0f5b0
Michael Harris <mhguitar1997@gmail.com>
2019-06-17 13:36:58 -0700

    Imported reducer and createStore from redux

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux'
+import {reducer} from './reducers';
+import {createStore} from 'redux';
 import './index.css';
 import App from './App';
 
