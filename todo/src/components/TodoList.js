55260e9997febc64af7679e504466369987fb302
Michael Harris <mhguitar1997@gmail.com>
2019-06-18 22:43:56 -0700

    Added CDM to hide toggle tip after 5 sec

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -4,9 +4,20 @@ import { connect } from "react-redux";
 import Todo from "./Todo";
 
 class TodoList extends React.Component {
+  state = {
+    hide: false
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({ hide: true})
+    },5000)
+  }
+
   render() {
     return (
       <div className="todo-list">
+        <h4 className={`${this.state.hide ? 'hide' : ''}`}>Click todo item to mark as completed.</h4>
         {this.props.todos.map(todo => (
           <Todo todo={todo} key={todo.id} />
         ))}
