7ef754daac121ac91e51f3c2bed58ecdc638d162
Michael Harris <mhguitar1997@gmail.com>
2019-06-18 21:46:51 -0700

    Added delete button on todo component

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
-import { toggleTodo, deleteTodo} from '../actions'
-
+import { toggleTodo, deleteTodo } from "../actions";
 
 const Todo = props => {
-  const {toggleTodo, todo} = props;
+  const { toggleTodo, deleteTodo, todo } = props;
   return (
-    <div className={`todo ${todo.completed ? 'completed' : ''}`}>
-      <h3>{todo.value}</h3><button onClick={() => toggleTodo(todo.id)}>Done</button>
+    <div className={`todo ${todo.completed ? "completed" : ""}`}>
+      <h3>{todo.value}</h3>
+      <button onClick={() => toggleTodo(todo.id)}>Done</button>
+      <button onClick={() => deleteTodo(todo.id)}>Delete</button>
     </div>
   );
 };
 
-export default connect(null, {toggleTodo, deleteTodo})(Todo);
+export default connect(
+  null,
+  { toggleTodo, deleteTodo }
+)(Todo);
